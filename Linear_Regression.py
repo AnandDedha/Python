d990c05a378312c15fcdf366c6e3d0e6342b07ec
AnandDedha <ananddedha@gmail.com>
2017-02-14 21:13:56 -0500

    Linear Regression

---
+++ Linear_Regression.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+customers = pd.read_csv("Ecommerce Customers")
+
+customers.head()
+customers.describe()
+
+customers.info()
+
+sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
+sns.jointplot(x='Time on App',y='Yearly Amount Spent',data=customers)
+
+sns.jointplot(x='Time on App',y='Length of Membership',kind='hex',data=customers)
+
+sns.pairplot(customers)
+
+sns.lmplot(x='Length of Membership',y='Yearly Amount Spent',data=customers)
+
+y = customers['Yearly Amount Spent']
+
+X = customers[['Avg. Session Length', 'Time on App','Time on Website', 'Length of Membership']]
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression()
+
+lm.fit(X_train,y_train)
+
+print( lm.coef_)
+
+
+predictions = lm.predict( X_test)
+
+plt.scatter(y_test,predictions)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
+
+from sklearn import metrics
+
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
